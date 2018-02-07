1ffc5d91dd01acca90327170147c90391e3ab009
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-07 07:08:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,7 +18,7 @@ function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
-while (maybeTrue()) {
+while (maybeTrue(array.length > 0 && maybeTrue())) {
   console.log("And I ran; I ran so far away!");
 }
 }
