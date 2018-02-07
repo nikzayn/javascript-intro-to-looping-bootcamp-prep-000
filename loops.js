02e19a0862c758addf6e59e6ce49e902e2515d91
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-07 07:22:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,7 @@ function doWhileLoop(array){
   return Math.random() >= 0.5
 }
 do {
-  console.log('doo-bee-doo-bee');
+  console.log('doo');
 }
 while (maybeTrue(array.length > 0 && maybeTrue()));
 return array
